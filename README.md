5e0401e227c1291e63331d5477bcc8010cfe34f6
Alexander Wunschik <alex@wunschik.net>
2023-02-12 16:56:07 +0100

    feat: constructor and basic functions

--- README.md
+++ README.md
@@ -1,2 +1,76 @@
 # joy-it-RB-LCD-20x4
-Accessing the LCD and input buttons of the RB-LCD-20x4 display by joy-it
+
+This is JavaScript/TypeScript library to access the LCD and input buttons of special LC-displays.
+Supported are some displays by [joy-it](https://joy-it.net) with a PCF8574/PCF8574AT i2c adapter chip.
+
+## Setup
+
+1. Plug in the display-shield to your raspberry pi like shown in user manuals.
+2. To access the display you first need to enable the i2c kernel module using `raspi-config`. Have a look in the manual (links below) for details.
+3. install node.js on the raspberry pi e.g. by using the official install script:
+
+```
+	$ curl -sL https://deb.nodesource.com/setup_18.x | sudo -E bash -
+	$ sudo apt install nodejs -y
+```
+
+4. Install this library with `npm install --save joy-it-rb-lcd-20x4`
+5. Run an example script
+
+## Example
+
+```js
+const { JoyItLCD } = require('joy-it-rb-lcd-20x4');
+
+async function main() {
+	cont lcd = new JoyItLCD();
+	await lcd.initialize();
+
+	await lcd.clear();
+	await lcd.printLine(0, '********************');
+	await lcd.printLine(1, '******* HELLO ******');
+	await lcd.printLine(2, '******* WORLD ******');
+	await lcd.printLine(3, '********************');
+}
+
+(async () => {
+	main();
+})();
+```
+
+## Supported Displays
+
+### RB-LCD-20x4
+
+The [RB-LCD-20x4](https://joy-it.net/en/products/RB-LCD-20x4) is a green monochrome 20x4 character LC-display with 4 additional buttons.
+
+- [RB-LCD-16x2-20x4 Manual English (PDF)](https://joy-it.net/files/files/Produkte/RB-LCD-20x4/RB-LCD-16x2-20x4_Manual_2022-02-22.pdf)
+- [Python example code](https://github.com/joy-it/PCF8574-LCD)
+
+```
+Pin:  7 |  6 |  5 |  4 |  3 |  2 |  1 |  0
+Hex: 80 | 40 | 20 | 10 | 08 | 04 | 02 | 01
+Use: EN | -  | RW | RS | D7 | D6 | D5 | D4
+```
+
+![product image of the RB-LCD-20x4](https://joy-it.net/files/files/Produkte/RB-LCD-20x4/RB-LCD20x4-01.png)
+
+### RB-LCD-16x2
+
+The [RB-LCD-16x2](https://joy-it.net/en/products/RB-LCD-16x2) should also work but support is untested.
+
+## Unsupported joy-it displays
+
+While this library could be used to access other displays with a PCF8574/PCF8574AT i2c adapter chip we recommend to use the [raspberrypi-liquid-crystal](https://www.npmjs.com/package/raspberrypi-liquid-crystal) library to access these displays.
+
+- [SBC-LCD16x2](https://joy-it.net/en/products/SBC-LCD16x2) - I2C Serial 2.6“ LCD Module
+- [SBC-LCD20x4](https://joy-it.net/en/products/SBC-LCD20x4) - I2C Serial 20x4 2004 LCD Module
+
+Displays without an i2c adapter are not supported at all by this class of libraries. These are for example:
+
+- [COM-LCD16X2](https://joy-it.net/en/products/com-LCD16X2)
+- [COM-LCD20x4](https://joy-it.net/en/products/COM-LCD20x4)
+
+## License
+
+This library is released under the [GPL-3.0-or-later](./LICENSE.md) license.
---
+++ index.js
@@ -0,0 +1,164 @@
+/**
+ * @author: Alexander Wunschik <dev@wunschik.net>
+ * @license: GPL-3.0-or-later
+ */
+
+const LCD = require('raspberrypi-liquid-crystal');
+const sleep = require('sleep');
+
+/**
+ * @typedef {Object} LCDOptions
+ */
+const DEFAULT_OPTIONS = {
+	bus: 1,
+	address: 0x27,
+	width: 20,
+	height: 4,
+};
+
+/**
+ * @class JoyItLCD
+ */
+class JoyItLCD {
+	/**
+	 * @type {LCDOptions}
+	 */
+	options = {};
+
+	/**
+	 * @type {LCD}
+	 */
+	lcd;
+
+	constructor(options = {}) {
+		this.options = {
+			...DEFAULT_OPTIONS,
+			...options,
+		};
+
+		this.lcd = new LCD(
+			this.options.bus,
+			this.options.address,
+			this.options.width,
+			this.options.height
+		);
+
+		/*
+		 * The joy-it LCD modules use a different pin mapping than the default one.
+		 */
+		this.lcd.displayPorts = {
+			D4: 0x01, // not used
+			D5: 0x02, // not used
+			D6: 0x04, // not used
+			D7: 0x08, // not used
+
+			RS: 0x10,
+			RW: 0x20, // not used
+			backlight: 0x40, // not available on the RB-20x4-LCD
+			E: 0x80,
+
+			CHR: 0x10,
+			CMD: 0x00,
+		};
+
+		/*
+		 * Because the joy-it LCD modules use a different pin mapping than the default one,
+		 * we have to overwrite the default _write4Sync and _write4Async functions to
+		 * use the lower nibble instead of the upper 4 bits the default functions use.
+		 */
+		this.lcd._write4Sync = function (x, c) {
+			const a = (x >> 4) & 0x0f; // Use lower 4 bit nibble
+			this._i2c.sendByteSync(this._address, a | this.displayPorts.backlight | c);
+			this._i2c.sendByteSync(
+				this._address,
+				a | this.displayPorts.E | this.displayPorts.backlight | c
+			);
+			this._i2c.sendByteSync(this._address, a | this.displayPorts.backlight | c);
+			sleep.usleep(2000);
+			return this;
+		};
+		this.lcd._write4Async = function (x, c, cb) {
+			const a = (x >> 4) & 0x0f; // Use lower 4 bit nibble
+			this._sendByte(a | this.displayPorts.backlight | c)
+				.then(() => this._sendByte(a | this.displayPorts.E | this.displayPorts.backlight | c))
+				.catch(e => {
+					if (cb) {
+						cb(e);
+					}
+				})
+				.then(() => this._sendByte(a | this.displayPorts.backlight | c))
+				.catch(e => {
+					if (cb) {
+						cb(e);
+					}
+				})
+				.then(
+					() => {
+						if (cb) {
+							cb();
+						}
+					},
+					e => {
+						if (cb) {
+							cb(e);
+						}
+					}
+				);
+		};
+	}
+
+	/**
+	 * Initializes the interface to the LCD screen.
+	 * Has to be called before any command.
+	 *
+	 * @async
+	 * @returns {Promise<void>}
+	 */
+	async initialize() {
+		return this.lcd.begin();
+	}
+
+	/**
+	 * Clears the LCD screen and positions the cursor in the upper-left corner.
+	 *
+	 * @async
+	 * @returns {Promise<void>}
+	 */
+	async clear() {
+		return this.lcd.clear();
+	}
+
+	/**
+	 * Positions the cursor in the upper-left of the LCD.
+	 *
+	 * @async
+	 * @returns {Promise<void>}
+	 */
+	async home() {
+		return this.lcd.home();
+	}
+
+	/**
+	 * Positions the LCD cursor.
+	 *
+	 * @async
+	 * @param {number} row The row to position the cursor in. Starting with 0.
+	 * @param {number} col The column to position the cursor in. Starting with 0.
+	 * @returns {Promise<void>}
+	 * @throws {Error} If the row or column are out of bounds.
+	 */
+	async setCursor(row, col) {
+		if (row < 0 || row > this.options.height - 1) {
+			throw new Error(`Invalid row "${row}". Must be within [0..${this.options.height - 1}]`);
+		}
+		if (col < 0 || col > this.options.width - 1) {
+			throw new Error(`Invalid column "${col}". Must be within [0..${this.options.width - 1}]`);
+		}
+		return this.lcd.setCursor(col, row);
+	}
+}
+
+module.exports = {
+	DEFAULT_OPTIONS,
+	JoyItLCD,
+};
